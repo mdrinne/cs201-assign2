f68d0700b72a3ee4e73277cd93ea8ce337ba40fa
Matthew Rinne <mdrinne@att.net>
2018-04-16 01:17:52 -0500

    fixed trees timing issues by making separate clean functions for strings and tokens

--- trees.c
+++ trees.c
@@ -15,19 +15,14 @@
 
 extern void avlRead(AVL *tree, int argc, char **argv);
 extern void gstRead(GST *tree, int argc, char **argv);
-// extern void cleanToken(char *);
-// extern void cleanString(char *);
-extern void cleaner(char *);
+extern void cleanToken(char *);
+extern void cleanString(char *);
 static void skipWhiteSpace(FILE *fp);
-// extern STRING *newSTRING(char *x);
 
 
 int
 main(int argc, char **argv)
 {
-  // static int(*compare)(void *, void *);
-  // static void (*display)(void *, FILE *);
-  // static void (*release)(void *);
 
   int type = 0;
 
@@ -66,16 +61,26 @@ avlRead(AVL *tree, int argc, char **argv)
   FILE *fp = fopen(argv[argc-2], "r");
   int sot = stringPending(fp);
   char *temp;
-  if (sot == 0) temp = readToken(fp);
-  else temp = readString(fp);
-  if (temp) cleaner(temp);
+  if (sot == 0) {
+    temp = readToken(fp);
+    if (temp) cleanToken(temp);
+  }
+  else {
+    temp = readString(fp);
+    if (temp) cleanString(temp);
+  }
   while (!feof(fp)) {
     STRING *temp2 = newSTRING(temp);
     if (isalpha(temp[0])) insertAVL(tree, temp2);
     sot = stringPending(fp);
-    if (sot == 0) temp = readToken(fp);
-    else temp = readString(fp);
-    if (temp) cleaner(temp);
+    if (sot == 0) {
+      temp = readToken(fp);
+      if (temp) cleanToken(temp);
+    }
+    else {
+      temp = readString(fp);
+      if (temp) cleanString(temp);
+    }
   }
   fclose(fp);
 
@@ -93,27 +98,42 @@ avlRead(AVL *tree, int argc, char **argv)
 
       case 'i':
         sot = stringPending(fp2);
-        if (sot == 0) temp = readToken(fp);
-        else temp = readString(fp);
-        if (temp) cleaner(temp);
+        if (sot == 0) {
+          temp = readToken(fp);
+          if (temp) cleanToken(temp);
+        }
+        else {
+          temp = readString(fp);
+          if (temp) cleanString(temp);
+        }
         STRING *temp3 = newSTRING(temp);
         if (isalpha(temp[0])) insertAVL(tree, temp3);
         break;
 
       case 'd':
         sot = stringPending(fp2);
-        if (sot == 0) temp = readToken(fp);
-        else temp = readString(fp);
-        if (temp) cleaner(temp);
+        if (sot == 0) {
+          temp = readToken(fp);
+          if (temp) cleanToken(temp);
+        }
+        else {
+          temp = readString(fp);
+          if (temp) cleanString(temp);
+        }
         STRING *temp4 = newSTRING(temp);
         if (isalpha(temp[0])) deleteAVL(tree, temp4);
         break;
 
       case 'f':
         sot = stringPending(fp2);
-        if (sot == 0) temp = readToken(fp);
-        else temp = readString(fp);
-        if (temp) cleaner(temp);
+        if (sot == 0) {
+          temp = readToken(fp);
+          if (temp) cleanToken(temp);
+        }
+        else {
+          temp = readString(fp);
+          if (temp) cleanString(temp);
+        }
         STRING *temp5 = newSTRING(temp);
         int freq = findAVLcount(tree,temp5);
         printf("Frequency of ");
@@ -133,28 +153,32 @@ avlRead(AVL *tree, int argc, char **argv)
 extern void
 gstRead(GST *tree, int argc, char **argv)
 {
-  // printf("--------in gstRead--------\n\n");
   FILE *fp = fopen(argv[argc-2], "r");
-  // printf("--------opened .data--------\n\n");
   int sot = stringPending(fp);
   char *temp;
-  if (sot == 0) temp = readToken(fp);
-  else temp = readString(fp);
-  if (temp) cleaner(temp);
-  // printf("--------cleaned--------\n\n");
+  if (sot == 0) {
+    temp = readToken(fp);
+    if (temp) cleanToken(temp);
+  }
+  else {
+    temp = readString(fp);
+    if (temp) cleanString(temp);
+  }
   while (!feof(fp)) {
     STRING *temp2 = newSTRING(temp);
     if (isalpha(temp[0])) insertGST(tree, temp2);
-    // displaySTRING(temp2, stdout);
-    // printf("\n--------inserted--------\n\n");
     sot = stringPending(fp);
-    if (sot == 0) temp = readToken(fp);
-    else temp = readString(fp);
-    if (temp) cleaner(temp);
+    if (sot == 0) {
+      temp = readToken(fp);
+      if (temp) cleanToken(temp);
+    }
+    else {
+      temp = readString(fp);
+      if (temp) cleanString(temp);
+    }
   }
-  // printf("--------left while loop--------\n\n");
   fclose(fp);
-  // printf("!!!!!!!!!!!!!!!!!!!END OF FIRST FILE!!!!!!!!!!!!!!!!!!!\n\n");
+
   FILE *fp2 = fopen(argv[argc-1], "r");
   char c = fgetc(fp2);
   while (!feof(fp2)) {
@@ -169,27 +193,42 @@ gstRead(GST *tree, int argc, char **argv)
 
       case 'i':
         sot = stringPending(fp2);
-        if (sot == 0) temp = readToken(fp);
-        else temp = readString(fp);
-        if (temp) cleaner(temp);
+        if (sot == 0) {
+          temp = readToken(fp);
+          if (temp) cleanToken(temp);
+        }
+        else {
+          temp = readString(fp);
+          if (temp) cleanString(temp);
+        }
         STRING *temp3 = newSTRING(temp);
         if (isalpha(temp[0])) insertGST(tree, temp3);
         break;
 
       case 'd':
         sot = stringPending(fp2);
-        if (sot == 0) temp = readToken(fp);
-        else temp = readString(fp);
-        if (temp) cleaner(temp);
+        if (sot == 0) {
+          temp = readToken(fp);
+          if (temp) cleanToken(temp);
+        }
+        else {
+          temp = readString(fp);
+          if (temp) cleanString(temp);
+        }
         STRING *temp4 = newSTRING(temp);
         if (isalpha(temp[0])) deleteGST(tree, temp4);
         break;
 
       case 'f':
         sot = stringPending(fp2);
-        if (sot == 0) temp = readToken(fp);
-        else temp = readString(fp);
-        if (temp) cleaner(temp);
+        if (sot == 0) {
+          temp = readToken(fp);
+          if (temp) cleanToken(temp);
+        }
+        else {
+          temp = readString(fp);
+          if (temp) cleanString(temp);
+        }
         STRING *temp5 = newSTRING(temp);
         // int freq'
         // if (isalpha(temp[0])) {
@@ -209,14 +248,43 @@ gstRead(GST *tree, int argc, char **argv)
 }
 
 
-extern void
-cleaner(char *str)
+static void
+skipWhiteSpace(FILE *fp)
 {
-  // int length = strlen(str);
+  int ch;
+  while ((ch = fgetc(fp)) != EOF && isspace(ch))
+    continue;
+  if (ch != EOF) ungetc(ch,fp);
+}
+
+
+extern void
+cleanToken(char *str)
+{
+  int size = strlen(str);
+  int dest = 0;
+  for (int i=0; i<size; i++) {
+    if (isupper(str[i])) {
+      str[dest] = str[i]+32;
+      ++dest;
+    }
+    else {
+      str[dest] = str[i];
+      ++dest;
+    }
+  }
+  str[dest] = '\0';
+  return;
+}
+
+extern void
+cleanString(char *str)
+{
+  int size = strlen(str);
   int source = 0;
   int dest = 0;
   int space = 0;
-  while (str[source] != '\0') {
+  for (int i=0; i<size; i++) {
     if (islower(str[source])) {
       space = 0;
       str[dest] = str[source];
@@ -237,6 +305,7 @@ cleaner(char *str)
     }
     source++;
   }
+
   if (isspace(str[dest-1])) {
     str[dest-1] = '\0';
   }
@@ -245,87 +314,3 @@ cleaner(char *str)
   }
   return;
 }
-
-
-static void
-skipWhiteSpace(FILE *fp)
-{
-  int ch;
-  while ((ch = fgetc(fp)) != EOF && isspace(ch))
-    continue;
-  if (ch != EOF) ungetc(ch,fp);
-}
-
-
-// extern void
-// cleanToken(char *str)
-// {
-//   // int size = sizeof(str)/sizeof(char *);
-//   int size = strlen(str);
-//   int pos = 0;
-//   // printf("TOKEN SIZE : %d\nTOKEN: .%s.\n", size, str);
-//   for (int i=0; i<size; i++) {
-//     if (isalpha(str[i])) {
-//       if (isupper(str[i])) {
-//         str[pos] = str[i] + 32;
-//       }
-//       else {
-//         str[pos] = str[i];
-//       }
-//       pos ++;
-//     }
-//     else continue;
-//   }
-//   str[pos] = '\0';
-//   // printf("TOKEN AFTER CLEANING: .%s.\n", str);
-//   return;
-// }
-//
-// extern void
-// cleanString(char *str)
-// {
-//   // int size = sizeof(str)/sizeof(char *);
-//   int size = strlen(str);
-//   int pos = 0;
-//   int space = 0;
-//   // printf("STR SIZE : %d\nSTRING: .%s.\n", size, str);
-//   for (int i=0; i<size; i++) {
-//     if (str[i] == 32) {
-//       if (space != 0) {
-//         if (str[i+1] != 32) {
-//           // space = 1;
-//           str[pos] = str[i];
-//           pos ++;
-//         }
-//       }
-//       else {
-//         continue;
-//       }
-//     }
-//     else if (isalpha(str[i])) {
-//       if (isupper(str[i])) {
-//         str[pos] = str[i] + 32;
-//       }
-//       else {
-//         str[pos] = str[i];
-//       }
-//       space = 1;
-//       pos ++;
-//     }
-//     else continue;
-//   }
-//
-//   str[pos] = '\0';
-//   // printf("STRING AFTER CLEANING: .%s.\n", str);
-//   return;
-// }
-
-
-// extern STRING *
-// newSTRING(char *x)
-// {
-//   STRING *p = malloc(sizeof(STRING));
-//   assert(p != 0);
-//   p->value = x;
-//   return p;
-// }
